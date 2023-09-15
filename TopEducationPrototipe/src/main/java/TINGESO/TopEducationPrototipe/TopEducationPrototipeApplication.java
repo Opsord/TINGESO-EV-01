30551aa549e108849a88d8295b55d9f6090dea48
Andres Zelaya <andres.zelaya.d@hotmail.com>
2023-09-15 17:30:00 -0300

    Project creation
    Initialization of Spring Project

---
+++ TopEducationPrototipe/src/main/java/TINGESO/TopEducationPrototipe/TopEducationPrototipeApplication.java
@@ -0,0 +1,13 @@
+package TINGESO.TopEducationPrototipe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TopEducationPrototipeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TopEducationPrototipeApplication.class, args);
+	}
+
+}
---
+++ TopEducationPrototipe/src/test/java/TINGESO/TopEducationPrototipe/TopEducationPrototipeApplicationTests.java
@@ -0,0 +1,13 @@
+package TINGESO.TopEducationPrototipe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopEducationPrototipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

7e0233e9facb8ddfbed3004fbad01ce88e1e45f9
Andres Zelaya <andres.zelaya.d@hotmail.com>
2023-09-18 02:34:32 -0300

    Persistence layer advance

---
+++ TopEducation/src/main/java/TINGESO/TopEducation/repositories/StudentReposotiry.java
@@ -0,0 +1,9 @@
+package TINGESO.TopEducation.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentReposotiry extends CrudRepository<StudentReposotiry, Long>  {
+
+}

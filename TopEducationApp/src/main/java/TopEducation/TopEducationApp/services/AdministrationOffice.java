d9ab9d93cf220602c7a55165268dc819562b3d7b
Andres Zelaya <andres.zelaya.d@hotmail.com>
2023-09-25 11:21:58 -0300

    Duty by installments calculation

--- TopEducationApp/src/main/java/TopEducation/TopEducationApp/services/AdministrationOffice.java
+++ TopEducationApp/src/main/java/TopEducation/TopEducationApp/services/AdministrationOffice.java
@@ -3,10 +3,45 @@ package TopEducation.TopEducationApp.services;
 import TopEducation.TopEducationApp.entities.StudentEntity;
 import org.springframework.stereotype.Service;
 
+import java.time.LocalDate;
+
 @Service
 
 //Part of the business layer
 public class AdministrationOffice {
+
+    //Verify is a student is valid
+    public boolean isValidStudent(StudentEntity student) {
+        //Verify if the student´s parameters are valid
+
+        //Verify if the student has a valid rut
+        if (student.getRut() == null || student.getRut().isBlank()) {
+            return false;
+        }
+        //Verify if the student has a valid first name
+        if (student.getFirstName() == null || student.getFirstName().isBlank() ) {
+            return false;
+        }
+        //Verify if the student  last name
+        if (student.getLastName() == null || student.getLastName().isBlank()) {
+            return false;
+        }
+        //Verify if the student´s birthdate is valid
+        LocalDate currentDate = LocalDate.now();
+        if (student.getBirthDate() == null || student.getBirthDate().isAfter(currentDate)) {
+            return false;
+        }
+        //Verify if the student has a valid school type
+        if (!(student.getSchoolType() == 0 || student.getSchoolType() == 1 || student.getSchoolType() == 2)) {
+            return false;
+        }
+        //Verify if the student has a valid graduation year
+        int currentYear = java.time.LocalDate.now().getYear();
+        if (student.getGraduationYear() < 1900 || student.getGraduationYear() > currentYear) {
+            return false;
+        }
+        return true;
+    }
 
     //Enrollment cost -> 70.000 CLP
     private final int enrollmentCost = 70000;
@@ -14,15 +49,17 @@ public class AdministrationOffice {
     private final int annualDuty = 1500000;
     //Total cost      -> 1.570.000 CLP
 
-    //Calculate annual cost considering the enrollment cost and the annual duty
-    //If the payment is made in cash, it has a 50% discount
-    //If the payment is made in installments, it has a discount depending on
-    //the type of school and the years since graduation
-
     //Calculate the annual cost if the payment is made in cash
-    public int calculateAnnualCostCash(StudentEntity student) {
-        return (int) ((enrollmentCost + annualDuty) * 0.5);
+    public double calculateAnnualCostCash(StudentEntity student) {
+        double finalPrice = (enrollmentCost + annualDuty) * 0.5;
+        //Adding a validation to avoid fraud
+        if (isValidStudent(student)) {
+            return finalPrice;
+        } else {
+            return 0;
+        }
     }
+
     //Calculate the annual cost if the payment is made in installments
     public double calculateAnnualCostInstallments(StudentEntity student) {
         //Calculate discount depending on the type of school
@@ -38,15 +75,20 @@ public class AdministrationOffice {
 
         //Calculate discount depending on the years since graduation
         double yearsSinceGraduationDiscount;
-        //Getting the current year
-        int currentYear = java.time.LocalDate.now().getYear();
-        int yearsSinceGraduation =  currentYear - student.getGraduationYear();
+        //Getting the current date
+        LocalDate currentYear = LocalDate.now();
+        //Calculating how many years have passed since the student graduated
+        int yearsSinceGraduation = currentYear.getYear() - student.getGraduationYear();
+        //First range: 0 - 1 year
         if (yearsSinceGraduation < 1) {
             yearsSinceGraduationDiscount = 0.15;
+        //Second range: 1 - 2 years
         } else if (yearsSinceGraduation < 3) {
             yearsSinceGraduationDiscount = 0.08;
+        //Third range: 3 - 4 years
         } else if (yearsSinceGraduation < 5) {
             yearsSinceGraduationDiscount = 0.04;
+        //Fourth range: 5+ years
         } else {
             yearsSinceGraduationDiscount = 0;
         }
@@ -54,6 +96,14 @@ public class AdministrationOffice {
         //Calculate the total discount
         double totalDiscount = schoolTypeDiscount + yearsSinceGraduationDiscount;
         //Calculate the total cost
-        return (enrollmentCost + annualDuty - ((enrollmentCost + annualDuty) * totalDiscount));
+        double finalPrice = ((enrollmentCost + annualDuty) * (1 - totalDiscount));
+
+        //Adding a validation to avoid fraud
+        if (isValidStudent(student)) {
+            return finalPrice;
+        } else {
+            return 0;
+        }
     }
+
 }
--- TopEducationApp/src/test/java/TopEducation/TopEducationApp/AdministrationOfficeTest.java
+++ TopEducationApp/src/test/java/TopEducation/TopEducationApp/AdministrationOfficeTest.java
@@ -12,6 +12,7 @@ class AdministrationOfficeTest {
     AdministrationOffice administrationOffice = new AdministrationOffice();
 
     @Test
+    //Test for the calculateAnnualCostInstallments with correct values
     void calculateAnnualCostCash() {
         //Setting up the student
         student.setRut("20.000.000-2");
@@ -22,7 +23,169 @@ class AdministrationOfficeTest {
         student.setGraduationYear(2010);
 
         //Simulating the payment in cash
-        int annualCostCash = administrationOffice.calculateAnnualCostCash(student);
-        assertEquals(785000, annualCostCash, 0.0);  //785.000 CLP
+        double annualCostCash = administrationOffice.calculateAnnualCostCash(student);
+        assertEquals(785000, annualCostCash, 0.0);//785.000 CLP
     }
+
+    @Test
+        //Test for the calculateAnnualCostInstallments with incorrect RUT
+    void calculateAnnualCostCashEmptyRut() {
+        //Setting up the student
+        student.setRut("");
+        student.setFirstName("Aquiles");
+        student.setLastName("Baeza");
+        student.setBirthDate(LocalDate.of(1995, 10, 15));
+        student.setSchoolType(0);
+        student.setGraduationYear(2015);
+
+        //Simulating the payment in cash
+        double annualCostCash = administrationOffice.calculateAnnualCostCash(student);
+        assertEquals(0, annualCostCash, 0.0);
+    }
+
+    @Test
+    //Test for the calculateAnnualCostInstallments with empty first name
+    void calculateAnnualCostCashEmptyFirstName() {
+        //Setting up the student
+        student.setRut("20.000.666-2");
+        student.setFirstName("");
+        student.setLastName("Perez");
+        student.setBirthDate(LocalDate.of(1970, 1, 1));
+        student.setSchoolType(0);
+        student.setGraduationYear(2020);
+
+        //Simulating the payment in cash
+        double annualCostCash = administrationOffice.calculateAnnualCostCash(student);
+        assertEquals(0, annualCostCash, 0.0);
+    }
+
+    @Test
+    //Test for the calculateAnnualCostInstallments with empty last name
+    void calculateAnnualCostCashEmptyLastName() {
+        //Setting up the student
+        student.setRut("20.000.666-2");
+        student.setFirstName("Raul");
+        student.setLastName("");
+        student.setBirthDate(LocalDate.of(1999, 1, 20));
+        student.setSchoolType(0);
+        student.setGraduationYear(2010);
+
+        //Simulating the payment in cash
+        double annualCostCash = administrationOffice.calculateAnnualCostCash(student);
+        assertEquals(0, annualCostCash, 0.0);
+    }
+
+    @Test
+    //Test for the calculateAnnualCostInstallments with invalid birthdate
+    void calculateAnnualCostCashInvalidBirthDate() {
+        //Setting up the student
+        student.setRut("20.000.666-2");
+        student.setFirstName("Juan");
+        student.setLastName("Perez");
+        student.setBirthDate(LocalDate.of(2070, 10, 15));
+        student.setSchoolType(0);
+        student.setGraduationYear(2010);
+
+        //Simulating the payment in cash
+        double annualCostCash = administrationOffice.calculateAnnualCostCash(student);
+        assertEquals(0, annualCostCash, 0.0);
+    }
+
+    @Test
+    //Test for the calculateAnnualCostInstallments with invalid school type
+    void calculateAnnualCostCashInvalidSchoolType() {
+        //Setting up the student
+        student.setRut("20.000.666-2");
+        student.setFirstName("Juan");
+        student.setLastName("Perez");
+        student.setBirthDate(LocalDate.of(1970, 1, 1));
+        student.setSchoolType(4);
+        student.setGraduationYear(2010);
+
+        //Simulating the payment in cash
+        double annualCostCash = administrationOffice.calculateAnnualCostCash(student);
+        assertEquals(0, annualCostCash, 0.0);
+    }
+
+    @Test
+    //Test for the calculateAnnualCostInstallments with invalid graduation year
+    void calculateAnnualCostCashInvalidGraduationYear() {
+        //Setting up the student
+        student.setRut("20.000.666-2");
+        student.setFirstName("Juan");
+        student.setLastName("Perez");
+        student.setBirthDate(LocalDate.of(1970, 1, 1));
+        student.setSchoolType(0);
+        student.setGraduationYear(1600);
+
+        //Simulating the payment in cash
+        double annualCostCash = administrationOffice.calculateAnnualCostCash(student);
+        assertEquals(0, annualCostCash, 0.0);
+    }
+
+    @Test
+    //Test for the calculateAnnualCostInstallments with type 0 and range 3
+    void calculateAnnualCostInstallmentsType0Range3() {
+        //Setting up the student
+        student.setRut("20.000.000-2");
+        student.setFirstName("Juan");
+        student.setLastName("Perez");
+        student.setBirthDate(LocalDate.of(1990, 1, 1));
+        student.setSchoolType(0);
+        student.setGraduationYear(2020);
+
+        //Simulating the payment in installments
+        double annualCostInstallments = administrationOffice.calculateAnnualCostInstallments(student);
+        assertEquals(1193200, annualCostInstallments, 1.0);//1.193.200 CLP
+    }
+
+    @Test
+    //Test for the calculateAnnualCostInstallments with type 1 and range 2
+    void calculateAnnualCostInstallmentsType1Range2() {
+        //Setting up the student
+        student.setRut("20.000.000-2");
+        student.setFirstName("Armando");
+        student.setLastName("Casas");
+        student.setBirthDate(LocalDate.of(1990, 1, 1));
+        student.setSchoolType(1);
+        student.setGraduationYear(2021);
+
+        //Simulating the payment in installments
+        double annualCostInstallments = administrationOffice.calculateAnnualCostInstallments(student);
+        assertEquals(1287400, annualCostInstallments, 1.0);//1.287.400 CLP
+    }
+
+    @Test
+    //Test for the calculateAnnualCostInstallments with type 2 and range 1
+    void calculateAnnualCostInstallmentsType2Range1() {
+        //Setting up the student
+        student.setRut("20.000.000-2");
+        student.setFirstName("Armando");
+        student.setLastName("Casas");
+        student.setBirthDate(LocalDate.of(1990, 1, 1));
+        student.setSchoolType(2);
+        student.setGraduationYear(2023);
+
+        //Simulating the payment in installments
+        double annualCostInstallments = administrationOffice.calculateAnnualCostInstallments(student);
+        assertEquals(1334500, annualCostInstallments, 1.0);//1.334.500 CLP
+    }
+
+    @Test
+    //Test for the calculateAnnualCostInstallments with incorrect values
+    void calculateAnnualCostInstallmentsEmptyRut() {
+        //Setting up the student
+        student.setRut("");
+        student.setFirstName("");
+        student.setLastName("");
+        student.setBirthDate(LocalDate.of(1995, 6, 10));
+        student.setSchoolType(1);
+        student.setGraduationYear(2020);
+
+        //Simulating the payment in installments
+        double annualCostInstallments = administrationOffice.calculateAnnualCostInstallments(student);
+        assertEquals(0, annualCostInstallments, 0.0);
+    }
+
+
 }
